23320a64e1928f1759d727d6f5c1563fbe8bd86e
Daniela Xavier <danielamxsoares@gmail.com>
2024-01-16 10:27:41 -0300

    feat: criar estrutura do projeto

---
+++ src/main/java/com/generation/reflorescer/ReflorescerApplication.java
@@ -0,0 +1,13 @@
+package com.generation.reflorescer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflorescerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflorescerApplication.class, args);
+	}
+
+}
