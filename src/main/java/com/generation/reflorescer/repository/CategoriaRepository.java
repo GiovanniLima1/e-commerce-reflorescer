eb8b07c406dad8f1c0e33af69657b38d623cee84
LarissaPedroso <lary_santos2000@hotmail.com>
2024-01-18 15:56:24 -0300

    Feat: Implementar metodo de busca personalizada por nome

--- src/main/java/com/generation/reflorescer/repository/CategoriaRepository.java
+++ src/main/java/com/generation/reflorescer/repository/CategoriaRepository.java
@@ -1,9 +1,20 @@
 package com.generation.reflorescer.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.reflorescer.model.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	/*
+		-> interface = contrato, tudo que iremos passar aqui será obrigatório a implementação
+		-> é tipo a pesquisa que fazemos no MySQL (Select ...)
+		-> IgnoreCase = não importa se é maiusculo ou minusculo
+		-> estamos fazendo a assinatura do método pq a JPA pesquisa só pelo Id, então temos que fazer na 'mão'
+		-> Param = parametro que será buscado 
+	*/
 	
+	List<Categoria>findAllByNomeCategoriaContainingIgnoreCase(@Param("nomeCategoria") String nome);
 }
