29c837699c60cee33ede58bde584ec8bd068ac08
unknown <andressapatricio93@gmail.com>
2024-01-19 10:32:47 -0300

    Feat: Criar classe model para usuário

---
+++ src/main/java/com/generation/reflorescer/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.generation.reflorescer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Nome de usuário é obrigatório!")
+	private String nomeUsuario;
+	
+	@NotBlank(message = "O email é obrigatório!")
+	@Email(message = "O email deve ser válido!")
+	private String email;
+	
+	@NotBlank(message = "A senha é obrigatória!")
+	@Size(min = 8, message = "A senha deve conter no mínimo 8 caracteres!")
+	private String senha;
+	
+	@Size(max = 5000)
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}	
+}
