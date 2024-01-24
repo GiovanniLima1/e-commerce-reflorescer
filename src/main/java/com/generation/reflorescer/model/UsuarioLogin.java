d4e09a55565c25a896cd1ba79c7bb9376747c870
unknown <andressapatricio93@gmail.com>
2024-01-24 11:24:36 -0300

    Teste

---
+++ src/main/java/com/generation/reflorescer/model/UsuarioLogin.java
@@ -0,0 +1,49 @@
+package com.generation.reflorescer.model;
+
+public class UsuarioLogin {
+	
+	private Long id;
+	private String nomeUsuario;
+	private String email;
+	private String senha;
+	private String foto;
+	private String token;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getFoto() {
+		return foto;
+	}
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	public String getToken() {
+		return token;
+	}
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
